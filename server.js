9c6e79c47db314423d44ed83096c6f57ae301445
siddishczar <70487945+siddishczar@users.noreply.github.com>
2024-08-14 23:04:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,23 @@ const app = express();
 app.use(cors());
 
 const apiKey = 'ca9ff7e009d001484ed2e7fd4b83ecc9';
-const seriesId = 'UNRATE';
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Economic Data API. Use /api/economic-data to get economic data.');
+  res.send('Welcome to the Economic Data API. Use /api/economic-data?series_id=SERIES_ID to get economic data.');
 });
 
 app.get('/api/economic-data', async (req, res) => {
+  const seriesId = req.query.series_id;
+
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Query Parameters: ${JSON.stringify(req.query)}`);
+  console.log(`Series ID: ${seriesId}`);
+
+  if (!seriesId) {
+    return res.status(400).json({ error: 'Missing series_id query parameter' });
+  }
   try {
+
     const apiUrl = `https://api.stlouisfed.org/fred/series/observations?series_id=${seriesId}&api_key=${apiKey}&file_type=json`;
     const response = await fetch(apiUrl);
     const data = await response.json();
@@ -24,5 +33,5 @@ app.get('/api/economic-data', async (req, res) => {
   }
 });
 
-const PORT = 3001;
+const PORT = 3002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
